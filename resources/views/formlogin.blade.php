809ed998ebb4c7dd1e777162dc7ed7075a40dd2d
Ayu Kardina <ayukardina13@mhs.if.its.ac.id>
2016-08-03 15:51:20 +0700

    nambah layouts user (head, navbar)

--- resources/views/formlogin.blade.php
+++ resources/views/formlogin.blade.php
@@ -3,19 +3,7 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Heroic Features - Start Bootstrap Template</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="{{ URL::asset('css/bootstrap1.min.css') }}" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="{{ URL::asset('css/heroic-features.css') }}" rel="stylesheet">
+    @include('layouts.userHead')
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -32,38 +20,7 @@
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index">Home</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="agenda">Agenda Jurusan</a>
-                    </li>
-                    <li>
-                        <a href="jadwal">Pinjam Ruangan</a>
-                    </li>
-                    <li>
-                        <a href="lihatdosen">Lihat Dosen</a>
-                    </li>
-                    
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="formlogin">ADMIN</a>
-                    </li>
-                    <li>
-                        <a href="logindosen">DOSEN</a>
-                    </li>
-                </ul>
-            </div>
+            @include('layouts.userNavbar')
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
