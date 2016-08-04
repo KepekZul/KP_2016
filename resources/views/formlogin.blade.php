77151d28be7655fc94e70da205b4e2152961f574
Ayu Kardina <ayukardina13@mhs.if.its.ac.id>
2016-08-04 12:52:59 +0700

    benerin tampilan login

--- resources/views/formlogin.blade.php
+++ resources/views/formlogin.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 
-    @include('layouts.userHead')
+    @include('layouts.loginHead')
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
